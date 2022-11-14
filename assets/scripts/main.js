f03b69b16f761d12815ff15816198426c7869589
Mariel Chua <chuamkaye@gmail.com>
2022-11-14 12:12:49 -0800

    Completed Expose part of lab, updated main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -68,19 +68,42 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+
+  if(JSON.parse(localStorage.getItem('recipes')) != null){
+    return JSON.parse(localStorage.getItem('recipes'));
+  }
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  const fetchedRecipes = [];
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
   /**************************/
+  const newPromise = new Promise(promiseCallBack);
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
   /**************************/
+  async function promiseCallBack(resolve, reject){
+    for(let i = 0; i < RECIPE_URLS.length; i++){
+      try{
+        let fetchResponse = await fetch(RECIPE_URLS[i]);
+        let fetchJson = await fetchResponse.json();
+        fetchedRecipes.push(fetchJson);
+        if(i == RECIPE_URLS.length - 1){
+          saveRecipesToStorage(fetchedRecipes);
+        }
+        resolve(fetchedRecipes);
+      }
+      catch(error){
+        console.log(error);
+        reject(error);
+      }
+    }
+  }
   // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
   //            declared above
   // A5. TODO - Since we are going to be dealing with asynchronous code, create
